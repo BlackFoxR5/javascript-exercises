80bf20dbd9e4d5a0be4208bfa0880472a8906738
BlackFoxR5 <anshumayborah99@gmail.com>
2024-10-18 00:38:53 +0530

    completed in one go

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");    // Step 1: Split the string into an array of characters
+    let reverseArray = splitString.reverse();    // Step 2: Reverse the array
+    let joinArray = reverseArray.join("");    // Step 3: Join the array back into a string
+    return joinArray;    // Return the reversed string
 };
+
+// Test the function with ''
+console.log(reverseString("")); //empty string
+
 
 // Do not edit below this line
 module.exports = reverseString;
