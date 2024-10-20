0d060cec1007c564e2a12db46b1f49f2ee2239c6
BlackFoxR5 <anshumayborah99@gmail.com>
2024-10-20 23:56:48 +0530

    hardest so far

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+function removeFromArray(arr, ...itemsToRemove) {
+    return arr.filter(item => !itemsToRemove.includes(item));
+  }
+  
+  // Example usage
+  const result = removeFromArray([1, 2, 3], "1", 3);
+  console.log(result); // Output: [1, 3, 4]
+  
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
