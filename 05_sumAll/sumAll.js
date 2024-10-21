cff409ef36d6d50e4e8237c649e1877381a82f26
BlackFoxR5 <anshumayborah99@gmail.com>
2024-10-22 02:16:00 +0530

    added comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,25 @@
 const sumAll = function(x, y) {
-    let arr = [];
+    let arr = []; //declared outside loop so value of n is created in a single loop
     let sum = 0;
-    const min = Math.min(x, y);
-    const max = Math.max(x, y);
+    const min = Math.min(x, y); // Get the smaller value between x and y
+    const max = Math.max(x, y); // Get the larger value between x and y
 
     if (x < 0 || y < 0) {
-        return "ERROR"
+        return "ERROR" //check if any of the inputs are negative numbers, return error if -ve
     }
     if (!Number.isInteger(x) || !Number.isInteger(y)) {
-        return "ERROR"
+        return "ERROR" //check if the inputs are non integers, return error if not integers
     }
     if (typeof x !== 'number' || typeof y !== 'number') {
-        return "ERROR"
+        return "ERROR" //check if the inputs are non-numbers, return error if not numbers
     }
 
     for (n = min; n <= max; n++) {
         arr.push(n);
-        sum = sum + n;
+        sum = sum + n; //add each value of 'n' since it is inside the loop
     }
     return sum;
-    console.log(sum);
+    
 };
 sumAll(10, [90, 1]);
 
