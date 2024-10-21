6f2f3d30f5f36cd2aae26ff5d6b8bbd2af52c2bc
BlackFoxR5 <anshumayborah99@gmail.com>
2024-10-22 00:57:22 +0530

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let arr = [];
+    let sum = 0;
+    const min = Math.min(x, y);
+    const max = Math.max(x, y);
 
+    if (x < 0 || y < 0) {
+        return "ERROR"
+    }
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR"
+    }
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return "ERROR"
+    }
+
+    for (n = min; n <= max; n++) {
+        arr.push(n);
+        sum = sum + n;
+    }
+    return sum;
+    console.log(sum);
 };
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
