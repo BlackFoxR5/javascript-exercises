0ab6bf43b0f23369de4c4fd515734eb14308a195
BlackFoxR5 <anshumayborah99@gmail.com>
2024-10-23 12:15:05 +0530

    done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if (typeof year !== 'number' || !Number.isInteger(year)) {
+    //     return "Invalid input: year must be an integer.";
+    // }
 
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+leapYears(1600);
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
