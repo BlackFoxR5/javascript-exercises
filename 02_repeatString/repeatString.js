5ff7c9ed907ed4df2544e61e2b1c99178a00eb06
BlackFoxR5 <anshumayborah99@gmail.com>
2025-01-22 23:43:58 +0530

    not completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
 const repeatString = function() {
-
+    let result = '';
+    const num = Math.floor(Math.random() * 1000)
+    for (let i = 0; i < num; i++) {
+        result += 'odin';
+    }
+    return result;
 };
+
+console.log(repeatString());
+console.log(repeatString().length/3);
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
