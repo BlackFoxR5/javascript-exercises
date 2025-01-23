10f8e5a28b30310b6aab84d577a60ea9c4a9932e
BlackFoxR5 <anshumayborah99@gmail.com>
2025-01-24 00:26:30 +0530

    regex used

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
 const palindromes = function (str) {
-    const noPunc = str.replace(/[^a-zA-Z0-9]/g, '');
+    const noPunc = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase(); //use of regular expression(regex)
     const reverse = noPunc.split('').reverse().join('');
 
 
