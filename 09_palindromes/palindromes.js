ef180beb3cfef6b4233d823ae7f111275f9df526
BlackFoxR5 <anshumayborah99@gmail.com>
2025-01-23 23:14:33 +0530

    noPunc took more than an hour

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const noPunc = str.replace(/[^a-zA-Z0-9]/g, '');
+    const reverse = noPunc.split('').reverse().join('');
 
+
+
+    return reverse === noPunc;
 };
 
 // Do not edit below this line
