35d081bea61bcb800d6c6221659aa141ab00be42
BlackFoxR5 <anshumayborah99@gmail.com>
2025-01-23 20:23:02 +0530

    revisit .pow and factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  array.forEach((x) => {
+    total += x;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    let total = 1;
+  array.forEach((x) => {
+    total *= x;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let n = 1;
+  for (let i = 1; i <= number; i++) {
+     n = n * i;
+  }
+  return n;
 };
 
 // Do not edit below this line
