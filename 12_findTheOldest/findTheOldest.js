38feca0da60e5540385473e329bdb081c612fb2a
BlackFoxR5 <anshumayborah99@gmail.com>
2025-01-25 14:31:49 +0530

    not completed, revisit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = (array) => {
+    return array.reduce((oldestPerson, currentPerson) => {
+        const ageOfOldest = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        const ageOfCurrent = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    
+        return ageOfCurrent > ageOfOldest ? currentPerson : oldestPerson;
+    });
 };
 
 // Do not edit below this line
